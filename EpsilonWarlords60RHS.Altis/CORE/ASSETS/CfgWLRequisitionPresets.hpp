28b4e1618ccada970a1eccbc00b91cd346368a9f
Capwell <admin@axerius.net>
2018-12-11 21:19:30 +0000

    Warlords

--- EpsilonWarlords60RHS.Altis/CORE/ASSETS/CfgWLRequisitionPresets.hpp
+++ EpsilonWarlords60RHS.Altis/CORE/ASSETS/CfgWLRequisitionPresets.hpp
@@ -73,52 +73,210 @@ class CfgWLRequisitionPresets
             };
             class Vehicles
             {
+            // Wheeled
                 class rhsusf_m998_w_4dr
                 {
                     cost = 50;
                     requirements[]={};
                 };
                 class rhsusf_m1045_w {
-                    cost = 1000;
+                    cost = 1250;
                     requirements[]={};
-                }
+                };
                 class rhsusf_m1043_w_mk19 {
-                    cost = 2500;
+                    cost = 1600;
                     requirements[]={};
-                }
+                };
                 class rhsusf_m1043_w_m2 {
                     cost = 1400;
                     requirements[]={};
-                }
+                };
                 class rhsusf_m1025_w {
+                    cost = 500;
+                    requirements[]={};
+                };
+                class rhsusf_M1078A1P2_B_M2_WD_fmtv_usarmy {
+                    cost = 1550;
+                    requirements[]={};
+                };
+                class rhsusf_M1078A1P2_B_WD_fmtv_usarmy {
+                    cost = 650;
+                    requirements[]={};
+                };
+                class rhsusf_M977A4_REPAIR_BKIT_M2_usarmy_wd {
+                    cost = 750;
+                    requirements[]={};
+                };
+                class rhsusf_M977A4_AMMO_BKIT_M2_usarmy_wd {
+                    cost = 750;
+                    requirements[]={};
+                };
+                class rhsusf_M978A4_BKIT_usarmy_wd {
+                    cost = 750;
+                    requirements[]={};
+                };
+                class rhsusf_M1085A1P2_B_WD_Medical_fmtv_usarmy {
+                    cost = 750;
+                    requirements[]={};
+                };
+                class rhsusf_M1220_usarmy_wd {
+                    cost = 350;
+                    requirements[]={};
+                };
+                class rhsusf_M1220_M153_M2_usarmy_wd {
+                    cost = 1000;
+                    requirements[]={};
+                };
+                class rhsusf_M1220_M2_usarmy_wd {
+                    cost = 1000;
+                    requirements[]={};
+                };
+                class rhsusf_M1220_MK19_usarmy_wd {
+                    cost = 1500;
+                    requirements[]={};
+                };
+                class rhsusf_M1230_M2_usarmy_wd {
+                    cost = 1000;
+                    requirements[]={};
+                };
+                class rhsusf_M1230_MK19_usarmy_wd {
+                    cost = 1500;
+                    requirements[]={};
+                };
+                class rhsusf_M1230a1_usarmy_wd {
+                    cost = 1250;
+                    requirements[]={};
+                };
+                class rhsusf_M1232_usarmy_wd {
+                    cost = 350;
+                    requirements[]={};
+                };
+                class rhsusf_M1232_M2_usarmy_wd {
+                    cost = 1000;
+                    requirements[]={};
+                };
+                class rhsusf_M1232_MK19_usarmy_wd {
+                    cost = 1500;
+                    requirements[]={};
+                };
+                class rhsusf_M1237_M2_usarmy_wd {
+                    cost = 1000;
+                    requirements[]={};
+                };
+                class rhsusf_M1237_MK19_usarmy_wd {
+                    cost = 1500;
+                    requirements[]={};
+                };
+                class rhsusf_M142_usarmy_WD {
+                    cost = 20000;
+                    requirements[]={};
+                };
+            // Tracked
+                class rhsusf_m1a1aimwd_usarmy {
+                    cost = 5000;
+                    requirements[]={};
+                };
+                class rhsusf_m1a1aim_tuski_wd {
+                    cost = 6000;
+                    requirements[]={};
+                };
+                class rhsusf_m1a2sep1wd_usarmy {
+                    cost = 5000;
+                    requirements[]={};
+                };
+                class rhsusf_m1a2sep1tuskiwd_usarmy {
+                    cost = 6000;
+                    requirements[]={};
+                };
+                class rhsusf_m1a2sep1tuskiiwd_usarmy {
+                    cost = 7000;
+                    requirements[]={};
+                };
+                class RHS_M2A2_wd {
                     cost = 3000;
                     requirements[]={};
-                }
+                };
+                class RHS_M2A2_BUSKI_WD {
+                    cost = 4000;
+                    requirements[]={};
+                };
+                class RHS_M2A3_wd {
+                    cost = 3000;
+                    requirements[]={};
+                };
+                class RHS_M2A3_BUSKI_wd {
+                    cost = 4000;
+                    requirements[]={};
+                };
+                class RHS_M2A3_BUSKIII_wd {
+                    cost = 5000;
+                    requirements[]={};
+                };
+                class RHS_M6_wd {
+                    cost = 6000;
+                    requirements[]={};
+                };
+                class rhsusf_m113_usarmy_supply {
+                    cost = 750;
+                    requirements[]={};
+                };
+                class rhsusf_m113_usarmy {
+                    cost = 700;
+                    requirements[]={};
+                };
+                class rhsusf_m113_usarmy_M2_90 {
+                    cost = 1200;
+                    requirements[]={};
+                };
+                class rhsusf_m113_usarmy_M240 {
+                    cost = 1250;
+                    requirements[]={};
+                };
+                class rhsusf_m113_usarmy_medical {
+                    cost = 750;
+                    requirements[]={};
+                };
+                class rhsusf_m113_usarmy_MK19 {
+                    cost = 1700;
+                    requirements[]={};
+                };
+                class rhsusf_m113_usarmy_MK19_90 {
+                    cost = 1700;
+                    requirements[]={};
+                };
+                class rhsusf_m113_usarmy_unarmed {
+                    cost = 500;
+                    requirements[]={};
+                };
+                class rhsusf_m109_usarmy {
+                    cost = 10000;
+                    requirements[]={};
+                };
             };
             class Aircraft
             {
-                // Helicopters
+            // Helicopters
                 class RHS_AH64D_wd
                 {
                     cost = 6000;
-                    requirements[]={"A"};
+                    requirements[]={"H"};
                 };
                 class RHS_CH_47F
                 {
                     cost = 3500;
-                    requirements[]={"A"};
+                    requirements[]={"H"};
                 };
                 class RHS_UH60M
                 {
                     cost = 3000;
-                    requirements[]={"A"};
+                    requirements[]={"H"};
                 };
                 class RHS_UH60M2
                 {
                     cost = 4500;
-                    requirements[]={"A"};
+                    requirements[]={"H"};
                 };
-                // Planes
+            // Planes
                 class RHS_A10 
                 {
                     cost = 6500;
@@ -173,7 +331,7 @@ class CfgWLRequisitionPresets
                 class Box_NATO_AmmoVeh_F {
                     cost = 500;
                     requirements[]={};
-                }
+                };
             };
             class Defences
             {
@@ -197,16 +355,63 @@ class CfgWLRequisitionPresets
                     cost = 250;
                     requirements[]={};
                 };
+                class RHS_M119_WD {
+                    cost = 600;
+                    requirements[]={};
+                };
+                class RHS_M252_WD {
+                    cost = 300;
+                    requirements[]={};
+                };
             };
         };
         class EAST // --- assets available for OPFOR
         {
             class Infantry
             {
-                class B_Soldier_F // --- must be asset class name
-                {
-                    cost = 100; // --- Command Points required
-                    requirements[]={}; // --- dispositions required ("A" = airstrip, "H" = helipad, "W" = water (harbor))
+                class rhs_msv_emr_rifleman {
+                    cost = 100;
+                    requirements[]={};
+                };
+                class rhs_msv_emr_arifleman {
+                    cost = 150;
+                    requirements[]={};
+                };
+                class rhs_msv_emr_grenadier {
+                    cost = 125;
+                    requirements[]={};
+                };
+                class rhs_msv_emr_grenadier_rpg {
+                    cost = 250;
+                    requirements[]={};
+                };
+                class rhs_msv_emr_machinegunner {
+                    cost = 200;
+                    requirements[]={};
+                };
+                class rhs_msv_emr_aa {
+                    cost = 300;
+                    requirements[]={};
+                };
+                class rhs_msv_emr_at {
+                    cost = 300;
+                    requirements[]={};
+                };
+                class rhs_msv_emr_engineer {
+                    cost = 200;
+                    requirements[]={};
+                };
+                class rhs_msv_emr_medic {
+                    cost = 125;
+                    requirements[]={};
+                };
+                class rhs_msv_emr_marksman {
+                    cost = 150;
+                    requirements[]={};
+                };
+                class rhs_vdv_marksman_asval {
+                    cost = 300;
+                    requirements[]={};
                 };
             };
             class Vehicles
